d35f9c54929e6a520dc50faf19e7a4db2b62252c
Nonemoticoner <nonemoticoner@gmail.com>
2016-03-29 18:39:13 +0200

    setup

---
+++ index.js
@@ -0,0 +1,14 @@
+/**
+ * songsterr
+ * https://github.com/Nonemoticoner/songsterr
+ *
+ * Copyright (c) 2016 Nonemoticoner
+ * Licensed under the MIT license.
+ */
+var request = require('request');
+
+module.exports = {
+
+	// ...
+	
+};
